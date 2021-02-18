bdd8beb01fd8dc8d66bae717757e9f9af9acc8c3
Pasindu Praveen <pasindup@eteknowledge.com>
2021-02-18 18:03:55 +0530

    .

--- Pages/ManageUser/Create.cshtml.cs
+++ Pages/ManageUser/Create.cshtml.cs
@@ -14,18 +14,11 @@ namespace InventoryManagementSystem.Pages.ManageUser
     //[Authorize(Roles = "Admin")]
     public class CreateModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
-
         private readonly InventoryManagementSystem.Data.ApplicationDbContext _context;
 
-        public CreateModel(InventoryManagementSystem.Data.ApplicationDbContext context,
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+        public CreateModel(InventoryManagementSystem.Data.ApplicationDbContext context)
         {
             _context = context;
-            _userManager = userManager;
-            _signInManager = signInManager;
         }
 
         [BindProperty]
